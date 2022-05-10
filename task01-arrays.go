a083f46d589ba32ae8d5da1765944c8a09ce6167
ambri <bachukilabadze@gmail.com>
2022-05-10 18:33:48 +0400

    first solution

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,15 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
-	return
+	var sum float32
+	var count int
+	for i:= 0; i< len(input); i++ {
+		// fmt.Println(input[i])
+		if input[i]== 0{
+			continue
+		}
+		count++
+		sum += input[i]
+	}
+	return sum / float32(count)
 }
