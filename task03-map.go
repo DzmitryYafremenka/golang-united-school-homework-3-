0b82dd6c37bd01f1af9a94b1a443403a01e29510
IlyaGrushevsky <96526822+IlyaGrushevsky@users.noreply.github.com>
2022-06-08 15:58:37 +0300

    Add files via upload

--- task03-map.go
+++ task03-map.go
@@ -8,7 +8,7 @@ func sortMapValues(input map[int]string) (result []string) {
 		preresult = append(preresult, i)
 	}
 	sort.Ints(preresult)
-	for v := range preresult {
+	for _, v := range preresult {
 		result = append(result, input[v])
 	}
 	return result
