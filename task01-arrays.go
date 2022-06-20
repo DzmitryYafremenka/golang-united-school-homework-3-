ed2198705a2fa368eae548b2498cbd7ac332dfae
AndrejLoz <an.lozinski@gmail.com>
2022-06-20 23:43:05 +0200

    AllOk2

--- task01-arrays.go
+++ task01-arrays.go
@@ -4,14 +4,13 @@ package homework
 //Implement function that returns an average value of array (sum / N)
 //input -> [1,2,3,4,5,6]
 //output -> 3.5
-
 func average(input [15]float32) (result float32) {
 	var numUnits int = 0
 	var sum float32 = 0
 	for i := 0; i < len(input); i++ {
 		if input[i] > 0 {
 			sum += input[i]
-			numUnits += 1
+			numUnits++
 		}
 	}
 	return sum / float32(numUnits)
