4732f72aace0c032dcf62716b3a49a11f9d43603
AndrejLoz <an.lozinski@gmail.com>
2022-06-20 23:45:54 +0200

    AllOk2

---
+++ homework.go
@@ -0,0 +1,11 @@
+package homework
+
+import (
+	"fmt"
+)
+
+func homework() {
+	var myArr = [15]float32{1, 2, 3, 4, 5, 6}
+
+	fmt.Println(average(myArr))
+}
--- task02-slice.go
+++ task02-slice.go
@@ -1,5 +1,10 @@
 package homework
 
+// Task 2: Slices
+// function that returns the copy of the original slice in reverse order. The type of elements is int64.
+// Input -> (1, 2, 5, 15)
+// Output -> (15, 5, 2, 1)
+
 func reverse(input []int64) (result []int64) {
 	//Place your code here
 	return
