c8ab271e0d302958296270e0b02d8d2795630cc4
AndrejLoz <an.lozinski@gmail.com>
2022-06-21 23:42:04 +0200

    AllOkEnd

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,9 +1,10 @@
 package homework
 
-//Task 1: Arrays
-//Implement function that returns an average value of array (sum / N)
-//input -> [1,2,3,4,5,6]
-//output -> 3.5
+// Task 1: Arrays
+// Implement function that returns an average value of array (sum / N)
+// input -> [1,2,3,4,5,6]
+// output -> 3.5
+
 func average(input [15]float32) (result float32) {
 	var numUnits int = 0
 	var sum float32 = 0
